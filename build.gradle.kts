9e41bf855b4a2a8c1484c2cb79e9bcb8061c2afa
henrik242 <henrik242@users.noreply.github.com>
2019-04-26 09:46:27 +0200

    Add CommandLineProcessor + small refinements

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,16 @@ buildscript {
 
 plugins {
     kotlin("jvm") version "1.3.30"
-    id("org.jetbrains.kotlin.kapt") version "1.3.30"
+    id("org.jetbrains.kotlin.kapt") version "1.3.31"
     `maven-publish`
 }
 
 group = "no.synth.kotlin.plugins"
-version = "0.1"
+version = "0.1-SNAPSHOT"
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.30")
-    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.3.30")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.31")
+    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.3.31")
 
     implementation("com.google.auto.service:auto-service:1.0-rc5")
     kapt("com.google.auto.service:auto-service:1.0-rc5")
@@ -27,7 +27,7 @@ repositories {
 
 val jar by tasks.getting(Jar::class) {
     manifest {
-        attributes["Implementation-Title"] = "no.synth.kotlin.plugins.really-all-open"
+        attributes["Implementation-Title"] = "kotlin-really-allopen"
         attributes["Implementation-Version"] = version
     }
 }
@@ -38,4 +38,8 @@ publishing {
             from(components["java"])
         }
     }
+}
+
+kapt {
+    includeCompileClasspath = false
 }
--- src/main/kotlin/no/synth/kotlin/plugins/reallyallopen/ReallyAllOpenPlugin.kt
+++ src/main/kotlin/no/synth/kotlin/plugins/reallyallopen/ReallyAllOpenPlugin.kt
@@ -2,8 +2,12 @@ package no.synth.kotlin.plugins.reallyallopen
 
 import com.google.auto.service.AutoService
 import org.jetbrains.kotlin.com.intellij.mock.MockProject
+import org.jetbrains.kotlin.compiler.plugin.AbstractCliOption
+import org.jetbrains.kotlin.compiler.plugin.CliOption
+import org.jetbrains.kotlin.compiler.plugin.CommandLineProcessor
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.CompilerConfigurationKey
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.descriptors.Modality.FINAL
@@ -18,8 +22,7 @@ import org.jetbrains.kotlin.resolve.BindingContext
 class ReallyAllOpenRegistrar : ComponentRegistrar {
 
     override fun registerProjectComponents(project: MockProject, configuration: CompilerConfiguration) {
-
-        return registerExtension(project, ReallyAllOpenExtension())
+        registerExtension(project, ReallyAllOpenExtension())
     }
 }
 
@@ -32,14 +35,35 @@ class ReallyAllOpenExtension : DeclarationAttributeAltererExtension {
             currentModality: Modality,
             bindingContext: BindingContext,
             isImplicitModality: Boolean
-    ): Modality? {
+    ): Modality? =
+            if (currentModality != FINAL) {
+                null
+            } else if (!isImplicitModality && modifierListOwner.hasModifier(FINAL_KEYWORD)) {
+                FINAL // Explicit final
+            } else {
+                OPEN
+            }
+}
 
-        return if (currentModality != FINAL) {
-            null
-        } else if (!isImplicitModality && modifierListOwner.hasModifier(FINAL_KEYWORD)) {
-            FINAL // Explicit final
-        } else {
-            OPEN
-        }
+@AutoService(CommandLineProcessor::class)
+class ReallyAllOpenCommandLineProcessor : CommandLineProcessor {
+
+    override val pluginId: String = "no.synth.kotlin.plugins.kotlin-really-allopen"
+
+    override val pluginOptions: Collection<CliOption> = listOf(
+            CliOption(
+                    optionName = "enabled", valueDescription = "<true|false>",
+                    description = "whether to enable the plugin or not"
+            )
+    )
+
+    override fun processOption(option: AbstractCliOption, value: String, configuration: CompilerConfiguration) =
+            when (option.optionName) {
+                "enabled" -> configuration.put(KEY_ENABLED, value.toBoolean())
+                else -> configuration.put(KEY_ENABLED, true)
+            }
+
+    companion object {
+        val KEY_ENABLED = CompilerConfigurationKey<Boolean>("whether the plugin is enabled")
     }
 }
