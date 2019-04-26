cb5e229f34b9af9bee727ae5a2a50826665e41c0
henrik242 <henrik242@users.noreply.github.com>
2019-04-26 12:39:11 +0200

    Move plugin into separate folder, part2

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,17 +1,13 @@
-buildscript {
-    repositories {
-        jcenter()
-    }
-}
-
 plugins {
-    kotlin("jvm") version "1.3.30"
+    kotlin("jvm") version "1.3.31"
     id("org.jetbrains.kotlin.kapt") version "1.3.31"
     `maven-publish`
 }
 
 group = "no.synth.kotlin.plugins"
 version = "0.1-SNAPSHOT"
+
+val artifactName = "kotlin-really-allopen"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.31")
@@ -21,15 +17,14 @@ dependencies {
     kapt("com.google.auto.service:auto-service:1.0-rc5")
 }
 
-repositories {
-    jcenter()
-}
-
 val jar by tasks.getting(Jar::class) {
     manifest {
-        attributes["Implementation-Title"] = "kotlin-really-allopen"
-        attributes["Implementation-Version"] = version
+        attributes["Implementation-Title"] = artifactName
+        attributes["Implementation-Version"] = project.version
     }
+    exclude("META-INF/*kotlin_module")
+
+    baseName = artifactName
 }
 
 publishing {
