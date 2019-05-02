c036cc1a639b0e6a5bdbaa0119ae14fbcf3e4d8b
henrik242 <henrik242@users.noreply.github.com>
2019-05-02 09:13:39 +0200

    Release v0.4

--- plugin/src/main/kotlin/no/synth/reallyallopen/ReallyAllOpenPlugin.kt
+++ plugin/src/main/kotlin/no/synth/reallyallopen/ReallyAllOpenPlugin.kt
@@ -30,7 +30,7 @@ import org.jetbrains.kotlin.resolve.BindingContext
 
 const val groupId = "no.synth"
 const val artifactId = "kotlin-really-allopen"
-const val version = "0.4-SNAPSHOT" // Remember to set version in build.gradle.kts as well
+const val version = "0.4" // Remember to set version in build.gradle.kts as well
 
 open class ReallyAllOpenGradlePlugin : Plugin<Project> {
 
